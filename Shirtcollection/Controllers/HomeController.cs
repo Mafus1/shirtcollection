8b0d9cb87cef1089dd809c9f368dc9a9fdf02c6f
Mafus1 <fabian.a.maurer95@gmail.com>
2016-09-14 12:08:20 +0200

    first commit

---
+++ Shirtcollection/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shirtcollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("sommer.html")]
+        public ActionResult Sommer()
+        {
+            return View();
+        }
+
+        [Route("herbst.html")]
+        public ActionResult Herbst()
+        {
+            return View();
+        }
+
+        [Route("fruehling.html")]
+        public ActionResult Fruehling()
+        {
+            return View();
+        }
+
+        [Route("winter.html")]
+        public ActionResult Winter()
+        {
+            return View();
+        }
+
+        [Route("kontakt.html")]
+        public ActionResult Kontakt()
+        {
+            return View();
+        }
+    }
+}
